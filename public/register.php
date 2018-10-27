3483b40eea7f84d18d75a3da67e3faf9e4fba456
Xun Zhou <xun.zhou@lidl.com>
2018-10-27 16:44:16 +0200

    green: add register.php

--- public/register.php
+++ public/register.php
@@ -3,6 +3,13 @@
 declare(strict_types = 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-$userFilePath = __DIR__ . '/../data/users.json';
 
-echo 'OK';
+$userFilePath = __DIR__ . '/../data/users.json';
+$users = json_decode(file_get_contents($userFilePath), true);
+
+$emailInput = $_POST['email'];
+$passwordInput = $_POST['password'];
+
+var_dump($emailInput, $passwordInput);
+
+echo 'Registration done';
